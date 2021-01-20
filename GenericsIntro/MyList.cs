b204ede0e96877295be6bc9e3eab991b0e3be35d
ezgi kütük <ezgikutuk3@gmail.com>
2021-01-20 23:52:31 +0300

    Generic, Koleksiyon eklendi ve OOP'ye giriş yapıldı.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> // T tipinde bir liste 
+    {
+        T[] items;
+        //constructor
+        public MyList()//class new'lendiğinde otomatik çalışır.
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }//foreign key'ler primary keyden hemen sonra yazılır.
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }//adet
+
+        //CRUD (Create,Read,Update,Delete)
+    }
+}
