d494c23078b38a25c0293d3af1689a632656ff0a
ezgi kütük <ezgikutuk3@gmail.com>
2021-01-28 00:55:51 +0300

    GameProject eklendi

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if(gamer.BirthYear == 1995 && gamer.FirstName =="EZGİ" 
+                && gamer.LastName == "KÜTÜK" && gamer.IdentityNumber == 12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
